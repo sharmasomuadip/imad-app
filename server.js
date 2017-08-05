ba1bfac4513595330e5f6ca2e1c8178d483380a4
sharmasoumyadip <sharmasoumyadip@gmail.com>
2017-08-06 04:53:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/images/profile.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'images', 'profile.jpg'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
