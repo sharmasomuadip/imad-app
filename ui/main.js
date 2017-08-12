174198ba4fe1f4e855e777bc3d23dee94d6f6416
sharmasoumyadip <sharmasoumyadip@gmail.com>
2017-08-12 06:40:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 console.log("Neymar");
-var access = document.getElementById("Student");
+var access = document.getElementById("college-small-name");
 var code = access.innerHTML;
 code = code + " midnight";
 alert(code);
