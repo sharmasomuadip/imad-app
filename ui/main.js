7b9ad15ed8c2cc72e79145f67f9e20838776255b
sharmasoumyadip <sharmasoumyadip@gmail.com>
2017-09-18 16:36:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 window.onload = fun;
 function fun(){
-    var a = getElementById("college-small-name");
+    var a = getElementById(college-small-name);
     setInterval(function(){
         a.innerHTML = "IIT KGP";
         
